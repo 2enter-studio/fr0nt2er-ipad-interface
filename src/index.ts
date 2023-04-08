ec2778c85011bc03d405014f53df0edc6c6de7a3
davilivob <davilivob@gmail.com>
2023-04-08 21:03:25 +0800

    init

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+function preload(): void {
+
+}
+
+function setup(): void {
+  createCanvas(windowWidth * .7, windowHeight * .7);
+  background(0);
+}
+
+function draw(): void { }
---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.html", "./dist/**/*.html"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
