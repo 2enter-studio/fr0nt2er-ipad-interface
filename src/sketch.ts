3276c8a4ab58c543ab1b9bb80b3961974035f084
davilivob <davilivob@gmail.com>
2023-04-13 19:56:10 +0800

    temperately stop animate

--- src/sketch.ts
+++ src/sketch.ts
@@ -5,6 +5,9 @@ const container_dom = <HTMLDivElement>document.getElementById("p5-container");
 const canvas_width: number = container_dom.offsetWidth;
 const canvas_height: number = container_dom.offsetHeight;
 
+const frame_rate = 20;
+let offset_x: number = 0;
+
 // Constain variable for graphic generation
 const [circle_scale, dot_amount] = [1.8, 8];
 const circle_center_xy = [canvas_width / 2, canvas_height * circle_scale * 0.5 + (canvas_height * .4)]
@@ -23,6 +26,7 @@ const get_video_progress = () => {
     res.text().then((data) => {
       console.log(data)
       progress_value = parseFloat(data);
+      // progress_value += 0.00001;
     })
   }).catch(err => console.log(err))
 };
@@ -77,7 +81,7 @@ function send_osc() {
 
 // P5 Script
 const sketch = function(p: p5) {
-  function draw_graph(progress_value: number) {
+  function draw_graph() {
     const circle_center = p.createVector(...circle_center_xy);
     p.noFill();
     p.stroke(90, 255, 70, 250);
@@ -88,7 +92,10 @@ const sketch = function(p: p5) {
     for (let i = -10; i < dot_amount + 10; i++) {
       const line_length = unit_length;
       const v1 = p.createVector(
-        canvas_width * i / dot_amount - ((progress_value * 180000) % canvas_width),
+        // canvas_width * i / dot_amount - ((progress_value * 190000) % canvas_width),
+        // canvas_width * i / dot_amount - (((canvas_width / (dot_amount)) * (p.frameCount % frame_rate)) / frame_rate),
+        // canvas_width * i / dot_amount - (offset_x * (p.frameCount % frame_rate) / frame_rate),
+        canvas_width * i / dot_amount - offset_x,
         canvas_height * .35
       );
       const v2 = (circle_center.copy().sub(v1)).normalize().mult(canvas_height * circle_scale / 2 + line_length / 2)
@@ -100,9 +107,7 @@ const sketch = function(p: p5) {
       p.textSize(15)
       p.textSize(20)
       p.textAlign(p.CENTER, p.BOTTOM)
-      p.text((get_img_num()) + i - dot_amount / 2, p1.x, p1.y)
-
-      // p.line(circle_center.x, circle_center.y, circle_center.x - v2.x, circle_center.y - v2.y)
+      p.text(get_img_num() + i + 2 - dot_amount / 2, p1.x, p1.y)
     }
     p.stroke(255, 200);
     p.strokeWeight(1);
@@ -112,7 +117,7 @@ const sketch = function(p: p5) {
   p.setup = () => {
     p.colorMode(p.HSB);
     switch_img(0);
-    p.frameRate(5);
+    p.frameRate(frame_rate);
     main_canvas = p.createCanvas(canvas_width, canvas_height, "p2d");
     main_canvas.parent("p5-container");
   };
@@ -146,7 +151,7 @@ const sketch = function(p: p5) {
       console.log(progress_value);
     }
 
-    draw_graph(progress_value);
+    draw_graph();
     current_img_id = income_img_id;
   };
 };
